ad3ec20513f158f79565b52978457aeac615fe6f
gajendra <gajendra.gulgulia@tum.de>
2019-05-19 18:05:55 +0200

    removed unncessary files

--- mars_rover/src/say_hello/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include "hello.hpp"
-
-void Hello::say_hello(){
-    std::cout << "Hello cmake\n";
-    std::cout << "Hello rover from version " << SAY_HELLO_VERSION <<" \n";
-}
--- mars_rover/src/say_hello/hello.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef HELLO_H
-#define HELLO_H 
-#include <iostream>
-
-namespace Hello{
-    void say_hello();
-}
-#endif /* ifndef HELLO_H */
