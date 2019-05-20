d7e9eb0915f19a0c1acbb362120360613ea4d0c7
gajendra <gajendra.gulgulia@tum.de>
2019-05-20 23:37:30 +0200

    5 tests running correctly

--- unit_test/main_test.cpp
+++ unit_test/main_test.cpp
@@ -1,14 +1,67 @@
 #include <gtest/gtest.h>
-
 #include "../mars_rover/src/InitializeRovers.hpp"
 #include "../mars_rover/src/Rover.hpp"
+#include <utility>
 
-TEST(SampleTestCase, TestOneIsOne) {
-    EXPECT_EQ(1, 1);
+
+/*! creating a test fixture for testing the  functionalities of 
+    the mars rover so that we don't need to create and destroy 
+    the MarsRover::Rover object for testing every method provided 
+    by the MarsRover::Rover interface
+*/
+class RoverTest :public ::testing::Test{
+    
+    protected:
+        MarsRover::Rover* dummyRover;
+
+        virtual void SetUp(){
+            dummyRover = new MarsRover::Rover();
+        }
+
+        virtual void TearDown(){
+            delete dummyRover;
+            dummyRover = nullptr;
+        }
+};
+
+    
+//test for turning left
+TEST_F(RoverTest, TurnLeft){
+    
+    dummyRover->set_orientation("N");
+    dummyRover->rotate_left();
+    EXPECT_EQ("W", dummyRover->get_orientation());
 }
 
 
-GTEST_API_ int main(int argc, char **argv) {
+TEST_F(RoverTest, TurnRight){
+    
+    dummyRover->set_orientation("N");
+    dummyRover->rotate_right();
+    EXPECT_EQ("E", dummyRover->get_orientation());
+}
+
+TEST_F(RoverTest, SetRoverPosition){
+    dummyRover->set_position(1,1);
+    EXPECT_EQ(std::make_pair(1,1), dummyRover->get_curr_pos());
+}
+
+TEST_F(RoverTest, NumberOfStepsToMove){
+    EXPECT_EQ(5, dummyRover->get_num_steps_to_move("MMMMM"));
+}
+
+TEST_F(RoverTest, MoveForward){
+    
+    dummyRover->set_position(1,1);
+    dummyRover->set_orientation("E");
+    std::cout << " CURRENT ORIENTATION: " << dummyRover->get_orientation() << "\n\n";
+    dummyRover->move_forward();
+    dummyRover->print_curr_pos();
+    std::pair<int, int> result(2,1);
+    EXPECT_EQ(result, dummyRover->get_curr_pos());
+}
+
+int main(int argc, char **argv) {
     printf("Running main() from %s\n", __FILE__);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
